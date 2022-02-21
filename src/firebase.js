d3b8f41d0483968bb6cb2951a24bbff325c1f0c7
Henry Derbyshire <henry@htderbyshire.com>
2022-02-21 12:38:07 +0000

    Fixed bug with deploying to firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJi7NUzN8Kp9H9KDfXpVr1NqVhyFXZvOc",
+  authDomain: "wheres-waldo-react.firebaseapp.com",
+  projectId: "wheres-waldo-react",
+  storageBucket: "wheres-waldo-react.appspot.com",
+  messagingSenderId: "985883570026",
+  appId: "1:985883570026:web:6a637e9392d5aae9e43f13",
+  measurementId: "G-CX1MKPG1MN"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
